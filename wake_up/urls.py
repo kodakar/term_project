a68fbd5dd234677eb367cc593f6e745aced929bc
kodakar <yamato.0825@icloud.com>
2023-01-11 01:32:39 +0900

    ファイルの場所変えたり、その他諸々

--- wake_up/urls.py
+++ wake_up/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path, include
 
-from .views import IndexView
+from .views import IndexView, VideoView
+
+from . import views
 
 urlpatterns = [
     path('', IndexView.as_view()),
+    path('video', VideoView.as_view()),
+    path('video_feed', views.video_feed_view(), name="video_feed"),
 ]
