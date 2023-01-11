8c544f3a327358b4622e12dd7df73caced9a8c74
kodakar <yamato.0825@icloud.com>
2023-01-12 00:32:33 +0900

    自分の投稿を見れるようにした

--- accounts/views.py
+++ accounts/views.py
@@ -2,11 +2,23 @@ from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views import generic
 from django.views.generic import TemplateView
+from django.views.generic import ListView, DetailView
+
+from blog.models import Post
+
 
 class SignUpView(generic.CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'accounts/signup.html'
 
-class ProfileView(TemplateView):
+class ProfileView(ListView):
     template_name = 'accounts/profiel.html'
+    model = Post
+
+class myDetail(DetailView):
+    # 詳細表示するモデルを指定 -> `object`で取得可能
+    template_name = 'accounts/myPost_detail.html'
+    model = Post
+
+
